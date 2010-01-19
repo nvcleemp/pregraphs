ffb8e6e483d24daee3c1aeaeb4ba932a10e7ec9a
nvcleemp <nico.vancleemput@gmail.com>
2010-01-19 08:10:01 +0000

    Only perform flush when something is written to the stream

--- util.h
+++ util.h
@@ -20,7 +20,7 @@
 #define DEBUGMSG(msg) fprintf(stderr, "%s:%u %s\n", __FILE__, __LINE__, msg); fflush(stderr);
 
 
-#define DEBUGCONDITIONALMSG(condition, msg) if(condition) fprintf(stderr, "%s:%u %s\n", __FILE__, __LINE__, msg); fflush(stderr);
+#define DEBUGCONDITIONALMSG(condition, msg) if(condition){ fprintf(stderr, "%s:%u %s\n", __FILE__, __LINE__, msg); fflush(stderr);}
 
 #define DEBUGDUMP(var, format) fprintf(stderr, "%s:%u %s=" format "\n", __FILE__, __LINE__, #var, var); fflush(stderr);
 
